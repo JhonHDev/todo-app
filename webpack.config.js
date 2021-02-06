db34c1d72572db1313920e771c43d6f45252692c
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-02-06 11:47:11 -0500

    [Add]: Support for html files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,5 +8,23 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve( __dirname, 'dist' ),
     publicPath: ''
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        options: {
+          attributes: true,
+          minimize: false
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Boilerplate',
+      template: './src/index.html'
+    }),
+  ]
 }
