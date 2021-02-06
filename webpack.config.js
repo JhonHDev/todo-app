3efe9238e67f1202bbfe6518003b1d3ed8abf09b
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-02-06 12:06:32 -0500

    [Add]: Support for CSS and Sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -19,12 +20,20 @@ module.exports = {
           minimize: false
         }
       },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack Boilerplate',
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      ignoreOrder: false
     }),
   ]
 }
