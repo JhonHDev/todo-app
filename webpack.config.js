df8e774ef91124a17791bf9af06074a16079cc57
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-02-06 21:08:43 -0500

    [Add]: Support for old browsers with Babel

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
         test: /\.html$/i,
         loader: 'html-loader',
         options: {
