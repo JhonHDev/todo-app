54da98efb54c85f660f9aff7348894cae9b0cfd3
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-03-08 20:30:51 -0500

    [Add]: Feature to complete and delete tasks in the DOM

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import createTodo from './createTodo';
 import renderTodo from './renderTodo';
 import resetForm from './resetForm';
+import todoActions from './todoActions';
 
 let todoList = [];
 const form = document.getElementById('form');
 const formCheck = document.getElementById('formCheck');
+const todoContainer = document.getElementById('todoContainer');
 
 const getFormValues = (e) => {
   e.preventDefault();
@@ -26,6 +28,8 @@ const runApp = () => {
   formCheck.addEventListener('click', (e) => {
     e.target.classList.toggle('completed');
   });
+
+  todoContainer.addEventListener('click', todoActions);
 };
 
 export default runApp;
