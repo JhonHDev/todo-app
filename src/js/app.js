cc6408e486bf7bafbba4eeec117e2a6496b8fc0f
Jhon Esteban Herrera <estebanhz9919@gmail.com>
2021-03-09 11:40:49 -0500

    [Add]: TodoItem Actions in the todoList Array

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,55 @@
 import createTodo from './createTodo';
 import renderTodo from './renderTodo';
 import resetForm from './resetForm';
-import todoActions from './todoActions';
 
 let todoList = [];
 const form = document.getElementById('form');
 const formCheck = document.getElementById('formCheck');
 const todoContainer = document.getElementById('todoContainer');
+
+const deleteTodo = (id) => {
+  const todoId = Number(id);
+  todoList = todoList.filter((todo) => todo.id !== todoId);
+};
+
+const toggleTodoCompleted = (id) => {
+  const todoId = Number(id);
+
+  todoList = todoList.map((todo) => {
+    if (todo.id === todoId) {
+      const newTodo = {
+        ...todo,
+        completed: !todo.completed,
+      };
+      return newTodo;
+    }
+
+    return todo;
+  });
+};
+
+const todoActions = (e) => {
+  const element = e.target;
+  const { parentElement: todoItem } = element.parentElement;
+
+  const isCheckButton = element.classList.contains('check__icon');
+  const isDeleteButton = element.classList.contains('todo__delete');
+
+  let todoItemId;
+
+  if (isCheckButton) {
+    todoItemId = element.parentElement.parentElement.getAttribute('id');
+    toggleTodoCompleted(todoItemId);
+    element.parentElement.classList.toggle('completed');
+    todoItem.querySelector('p').classList.toggle('completed');
+  }
+
+  if (isDeleteButton) {
+    todoItemId = element.parentElement.getAttribute('id');
+    deleteTodo(todoItemId);
+    element.parentElement.remove();
+  }
+};
 
 const getFormValues = (e) => {
   e.preventDefault();
